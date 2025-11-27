2ba4169ab6a497200d53cf0fda3a5df24c3a8c5c
kwsu30 <50939503+kwsu30@users.noreply.github.com>
2025-11-27 14:49:42 +0800

    Update app.py

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from fastapi import FastAPI, UploadFile, HTTPException
+from fastapi import FastAPI, UploadFile, HTTPException, File
 from fastapi.responses import JSONResponse, FileResponse
 
 import cups 
