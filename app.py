dada18a0a885215bb3f25ad39d633539fd8288e7
kwsu30 <50939503+kwsu30@users.noreply.github.com>
2026-01-08 17:38:20 +0800

    Update app.py

--- app.py
+++ app.py
@@ -57,9 +57,7 @@ async def check_printer_list():
         raise HTTPException(status_code=500, detail=f"Error retrieving printer list: {str(e)}")
 
 @app.post("/print-pdf/")
-async def print_pdf(file: UploadFile = File(...), 
-                    printer: Form(None)
-                    ):
+async def print_pdf(file: UploadFile = File(...), printer: Form(None)):
     # Validate file type
     if file.content_type != "application/pdf":
         raise HTTPException(status_code=400, detail="Only PDF files are accepted")
